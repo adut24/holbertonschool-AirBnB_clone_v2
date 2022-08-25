db03d182babd87cb3ff4623035dbb5569713292f
adut24 <dutertre.alexandre@laposte.net>
2022-08-25 14:58:05 +0200

    Correction for puppet-lint

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,59 +1,11 @@
 # Set up Nginx
-exec {'update':
-  command => 'apt-get update',
-  path    => '/usr/bin/env',
-}
-
--> package {'nginx':
-  ensure  => 'present',
-  name    => 'nginx',
-  require => Exec['update'],
-}
-
--> exec {'mkdir1':
-  command => 'mkdir -p /data/web_static/releases/test',
-  path    => '/usr/bin/env',
-}
-
--> exec {'mkdir2':
-  command => 'mkdir -p /data/web_static/shared/',
-  path    => '/usr/bin/env',
-}
-
--> file {'echo':
-  ensure  => 'present',
-  path    => '/data/web_static/releases/test/index.html',
-  content => 'Fake file',
-  require => Exec['mkdir1'],
-}
-
--> exec {'rm':
-  command => 'rm -rf /data/web_static/current',
-  path    => '/usr/bin/env',
-  require => [Exec['mkdir1'], Exec['mkdir2']],
-}
-
--> exec {'ln':
-  command => 'ln -s /data/web_static/releases/test /data/web_static/current',
-  path    => '/usr/bin/env',
-  require => Exec['rm'],
-}
-
--> exec {'chown':
-  command => 'chown -R vagrant:vagrant /data/',
-  path    => '/usr/bin/env',
-  require => [Exec['mkdir1'], Exec['mkdir2']],
-}
-
--> file_line {'redirection':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  after   => 'server_name _;',
-  line    => 'location /hbnb_static { alias /data/web_static/current/; autoindex off; }',
-  require => Package['nginx'],
-}
-
--> service {'nginx':
-  ensure  => 'running',
-  require => Package['nginx'],
-}
+exec {'/usr/bin/env apt-get update': }
+-> exec {'/usr/bin/env apt-get -y install nginx': }
+-> exec {'/usr/bin/env mkdir -p /data/web_static/shared/': }
+-> exec {'/usr/bin/env mkdir -p /data/web_static/releases/test': }
+-> exec {'/usr/bin/env echo "Fake file" > /data/web_static/releases/test/index.html': }
+-> exec {'/usr/bin/env rm -rf /data/web_static/current': }
+-> exec {'/usr/bin/env ln -s /data/web_static/releases/test /data/web_static/current': }
+-> exec {'/usr/bin/env chown -R ubuntu:ubuntu /data/': }
+-> exec {'/usr/bin/env sed -i "47i location /hbnb_static { alias /data/web_static/current/; }" /etc/nginx/sites-available/default': }
+-> exec {'/usr/bin/env service nginx restart': }
