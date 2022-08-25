749eb9c4ea633731e0872f7b1a8461c12ae67931
adut24 <dutertre.alexandre@laposte.net>
2022-08-25 09:43:12 +0200

    Correction

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,11 +2,11 @@
 # Set up the web servers for the deployment of web_static
 apt update
 apt -y install nginx
-mkdir -p /data/web_static/releases/test
 mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test
 echo "Fake file" > /data/web_static/releases/test/index.html
 rm -rf /data/web_static/current
-ln -sT /data/web_static/releases/test /data/web_static/current
-chown -R "ubuntu":"ubuntu" /data/
-sed -i '/server_name _;/a \\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}' /etc/nginx/sites-available/default
+ln -s /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '/47i\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}' /etc/nginx/sites-available/default
 service nginx restart
